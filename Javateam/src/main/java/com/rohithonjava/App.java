aedb54281195ed1b9b64548d1ef0debcd8f8d52c
Rohith Reddy.G.K <gkrohithreddy@gmail.com>
2025-05-19 22:21:33 +0530

    Azure Pipeline Example.

---
+++ Javateam/src/main/java/com/rohithonjava/App.java
@@ -0,0 +1,13 @@
+package com.rohithonjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!! Welcome to DevOps Lab Programme 10." );
+    }
+}
